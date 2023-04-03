31f774cef4ae3c5726d8f46f360cca88286a3cac
rentezzy <ren2ren4ik@gmail.com>
2023-04-03 20:24:21 +0300

    Add gallery and fixed bugs

--- script.js
+++ script.js
@@ -15,6 +15,26 @@ function newsHandler() {
     const video = document.getElementById("news__cat__video");
     const play = document.getElementById("news_play");
     const pause = document.getElementById("news_pause");
+    const rightarrow = document.querySelector(".news__mainarrowright");
+    const leftarrow = document.querySelector(".news__mainarrowleft");
+    rightarrow.addEventListener("click", (e) => {
+        const images = document.querySelectorAll(".news__image");
+        const active = document.querySelector(".news__image_active");
+        const target = +active.dataset.index + 1 >= images.length
+            ? images[0]
+            : images[+active.dataset.index + 1];
+        active.classList.toggle("news__image_active");
+        target.classList.toggle("news__image_active");
+    });
+    leftarrow.addEventListener("click", (e) => {
+        const images = document.querySelectorAll(".news__image");
+        const active = document.querySelector(".news__image_active");
+        const target = +active.dataset.index - 1 >= 0
+            ? images[+active.dataset.index - 1]
+            : images[images.length - 1];
+        active.classList.toggle("news__image_active");
+        target.classList.toggle("news__image_active");
+    });
     play.addEventListener("click", (e) => {
         video.play();
     });
--- script.ts
+++ script.ts
@@ -18,6 +18,32 @@ function newsHandler() {
   const video = document.getElementById("news__cat__video") as HTMLVideoElement;
   const play = document.getElementById("news_play");
   const pause = document.getElementById("news_pause");
+  const rightarrow = document.querySelector(
+    ".news__mainarrowright"
+  ) as HTMLElement;
+  const leftarrow = document.querySelector(
+    ".news__mainarrowleft"
+  ) as HTMLElement;
+  rightarrow.addEventListener("click", (e) => {
+    const images = document.querySelectorAll(".news__image");
+    const active = document.querySelector(".news__image_active") as HTMLElement;
+    const target =
+      +active.dataset.index + 1 >= images.length
+        ? images[0]
+        : images[+active.dataset.index + 1];
+    active.classList.toggle("news__image_active");
+    target.classList.toggle("news__image_active");
+  });
+  leftarrow.addEventListener("click", (e) => {
+    const images = document.querySelectorAll(".news__image");
+    const active = document.querySelector(".news__image_active") as HTMLElement;
+    const target =
+      +active.dataset.index - 1 >= 0
+        ? images[+active.dataset.index - 1]
+        : images[images.length - 1];
+    active.classList.toggle("news__image_active");
+    target.classList.toggle("news__image_active");
+  });
   play.addEventListener("click", (e) => {
     video.play();
   });
@@ -31,7 +57,7 @@ showhide.addEventListener("click", (e) => {
   nav.classList.toggle("side_show");
 });
 burger.addEventListener("click", (e) => {
-  header.classList.toggle("_active")
+  header.classList.toggle("_active");
 });
 
 nav.addEventListener("click", async (event) => {
